7489313bb1f4cc6c3329a15b8f4d852abccd64ae
Mehdi Hasan <mhasan@omicronlab.com>
2012-09-01 07:06:57 +0600

    Removed synchronized blocks

--- src/com/omicronlab/avro/phonetic/dict/PhoneticParser.java
+++ src/com/omicronlab/avro/phonetic/dict/PhoneticParser.java
@@ -1,6 +1,6 @@
 /*
 	=============================================================================
-	*****************************************************************************
+ *****************************************************************************
 	The contents of this file are subject to the Mozilla Public License
 	Version 1.1 (the "License"); you may not use this file except in
 	compliance with the License. You may obtain a copy of the License at
@@ -21,9 +21,9 @@
 
 	Contributor(s): ______________________________________.
 
-	*****************************************************************************
+ *****************************************************************************
 	=============================================================================
-*/
+ */
 
 package com.omicronlab.avro.phonetic.dict;
 
@@ -37,7 +37,7 @@ import com.omicronlab.avro.phonetic.dict.models.Pattern;
 import com.omicronlab.avro.phonetic.dict.models.Rule;
 
 public class PhoneticParser {
-	
+
 	private static volatile PhoneticParser instance = null;
 	private static PhoneticLoader loader = null;
 	private static List<Pattern> patterns;
@@ -45,7 +45,7 @@ public class PhoneticParser {
 	private static String consonant = "";
 	private static String ignore = "";
 	private boolean initialized = false;
-	
+
 	// Prevent initialization
 	private PhoneticParser() {
 		patterns = new ArrayList<Pattern>();
@@ -54,154 +54,134 @@ public class PhoneticParser {
 	public Object clone() throws CloneNotSupportedException {
 		throw new CloneNotSupportedException();
 	}
-	
+
 	public static PhoneticParser getInstance() {
-		if(instance == null) {
-			synchronized (PhoneticParser.class) {
-				if(instance == null) {
-					instance = new PhoneticParser();
-				}
+		if (instance == null) {
+			if (instance == null) {
+				instance = new PhoneticParser();
 			}
 		}
 		return instance;
 	}
-	
-	public synchronized void setLoader(PhoneticLoader loader) {
+
+	public void setLoader(PhoneticLoader loader) {
 		PhoneticParser.loader = loader;
 	}
-	
-	public synchronized void init() throws Exception {
-		if(loader == null) {
+
+	public void init() throws Exception {
+		if (loader == null) {
 			new Exception("No PhoneticLoader loader available");
 		}
 		Data data = loader.getData();
 		patterns = data.getPatterns();
 		Collections.sort(patterns);
-		
+
 		vowel = data.getVowel();
 		consonant = data.getConsonant();
 		ignore = data.getIgnore();
 		initialized = true;
 	}
-	
+
 	public PhoneticResult parse(String input) {
-		
-		if(initialized == false) {
+
+		if (initialized == false) {
 			try {
 				this.init();
-			} catch(Exception e) {
+			} catch (Exception e) {
 				System.err.println(e);
 				System.err.println("Please handle the exception by calling init");
 				System.exit(0);
 			}
 		}
-		
+
 		String fixed = "";
-		for(char c: input.toCharArray()) {
-			if(!this.isIgnore(c)) {
+		for (char c : input.toCharArray()) {
+			if (!this.isIgnore(c)) {
 				fixed += Character.toLowerCase(c);
 			}
 		}
-		
-		//String output = "";
+
+		// String output = "";
 		PhoneticResult output = new PhoneticResult();
 		ArrayList<String> temPatterns = new ArrayList<String>();
-		
-		for(int cur = 0; cur < fixed.length(); ++cur) {
+
+		for (int cur = 0; cur < fixed.length(); ++cur) {
 			int start = cur, end = cur + 1, prev = start - 1;
 			boolean matched = false;
-			for(Pattern pattern: patterns) {
+			for (Pattern pattern : patterns) {
 				end = cur + pattern.getFind().length();
-				if(end <= fixed.length() && fixed.substring(start, end).equals(pattern.getFind())) {
+				if (end <= fixed.length() && fixed.substring(start, end).equals(pattern.getFind())) {
 					prev = start - 1;
-					for(Rule rule: pattern.getRules()) {
+					for (Rule rule : pattern.getRules()) {
 						boolean replace = true;
-						
+
 						int chk = 0;
-						
-						for(Match match: rule.getMatches()) {
-							if(match.getType().equals("suffix")) {
+
+						for (Match match : rule.getMatches()) {
+							if (match.getType().equals("suffix")) {
 								chk = end;
-							} 
+							}
 							// Prefix
 							else {
 								chk = prev;
 							}
-							
+
 							// Beginning
-							if(match.getScope().equals("punctuation")) {
-								if(
-									! (
-										(chk < 0 && match.getType().equals("prefix")) || 
-										(chk >= fixed.length() && match.getType().equals("suffix")) || 
-										this.isPunctuation(fixed.charAt(chk))
-									) ^ match.isNegative()
-								) {
+							if (match.getScope().equals("punctuation")) {
+								if (!((chk < 0 && match.getType().equals("prefix")) || (chk >= fixed.length() && match.getType().equals("suffix")) || this.isPunctuation(fixed
+										.charAt(chk))) ^ match.isNegative()) {
 									replace = false;
 									break;
 								}
 							}
 							// Vowel
-							else if(match.getScope().equals("vowel")) {
-								if(
-									! (
-										(
-											(chk >= 0 && match.getType().equals("prefix")) || 
-											(chk < fixed.length() && match.getType().equals("suffix"))
-										) && 
-										this.isVowel(fixed.charAt(chk))
-									) ^ match.isNegative()
-								) {
+							else if (match.getScope().equals("vowel")) {
+								if (!(((chk >= 0 && match.getType().equals("prefix")) || (chk < fixed.length() && match.getType().equals("suffix"))) && this.isVowel(fixed
+										.charAt(chk))) ^ match.isNegative()) {
 									replace = false;
 									break;
 								}
 							}
 							// Consonant
-							else if(match.getScope().equals("consonant")) {
-								if(
-									! (
-										(
-											(chk >= 0 && match.getType().equals("prefix")) || 
-											(chk < fixed.length() && match.getType().equals("suffix"))
-										) && 
-										this.isConsonant(fixed.charAt(chk))
-									) ^ match.isNegative()
-								) {
+							else if (match.getScope().equals("consonant")) {
+								if (!(((chk >= 0 && match.getType().equals("prefix")) || (chk < fixed.length() && match.getType().equals("suffix"))) && this.isConsonant(fixed
+										.charAt(chk))) ^ match.isNegative()) {
 									replace = false;
 									break;
 								}
 							}
 							// Exact
-							else if(match.getScope().equals("exact")) {
+							else if (match.getScope().equals("exact")) {
 								int s, e;
-								if(match.getType().equals("suffix")) {
+								if (match.getType().equals("suffix")) {
 									s = end;
 									e = end + match.getValue().length();
-								} 
+								}
 								// Prefix
 								else {
 									s = start - match.getValue().length();
 									e = start;
 								}
-								if(!this.isExact(match.getValue(), fixed, s, e, match.isNegative())) {
+								if (!this.isExact(match.getValue(), fixed, s, e, match.isNegative())) {
 									replace = false;
 									break;
 								}
 							}
 						}
-						
-						if(replace) {
+
+						if (replace) {
 							output.regex += rule.getReplace() + "(্[যবম])?(্?)([ঃঁ]?)";
 							temPatterns.add(rule.getReplace());
 							cur = end - 1;
 							matched = true;
 							break;
 						}
-						
+
 					}
 
-					if(matched == true) break;
-					
+					if (matched == true)
+						break;
+
 					// Default
 					output.regex += pattern.getReplace() + "(্[যবম])?(্?)([ঃঁ]?)";
 					temPatterns.add(pattern.getReplace());
@@ -210,39 +190,40 @@ public class PhoneticParser {
 					break;
 				}
 			}
-			
-			if(!matched) {
+
+			if (!matched) {
 				output.regex += fixed.charAt(cur);
 				temPatterns.add(fixed.charAt(cur) + "");
 			}
-			// System.out.printf("cur: %s, start: %s, end: %s, prev: %s\n", cur, start, end, prev);
+			// System.out.printf("cur: %s, start: %s, end: %s, prev: %s\n", cur,
+			// start, end, prev);
 		}
-		
-		if (temPatterns.size()>0){
-			output.regexScope = "^" + temPatterns.get(0) + ".*" +  temPatterns.get(temPatterns.size() - 1) + "(্[যবম])?(্?)([ঃঁ]?)" + "$";
+
+		if (temPatterns.size() > 0) {
+			output.regexScope = "^" + temPatterns.get(0) + ".*" + temPatterns.get(temPatterns.size() - 1) + "(্[যবম])?(্?)([ঃঁ]?)" + "$";
 		}
-		
+
 		return output;
 	}
-	
+
 	private boolean isVowel(char c) {
 		return ((vowel.indexOf(Character.toLowerCase(c)) >= 0));
 	}
-	
+
 	private boolean isConsonant(char c) {
 		return ((consonant.indexOf(Character.toLowerCase(c)) >= 0));
 	}
-	
+
 	private boolean isPunctuation(char c) {
 		return (!(this.isVowel(c) || this.isConsonant(c)));
 	}
-	
+
 	private boolean isExact(String needle, String heystack, int start, int end, boolean not) {
 		return ((start >= 0 && end < heystack.length() && heystack.substring(start, end).equals(needle)) ^ not);
 	}
-	
+
 	private boolean isIgnore(char c) {
 		return (ignore.indexOf(Character.toLowerCase(c)) >= 0);
 	}
-	
+
 }
